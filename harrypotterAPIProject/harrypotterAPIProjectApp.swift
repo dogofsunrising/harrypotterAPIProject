d76fda33012fc6ac8b9c42d5daab900e2ef61eed
dogofsunrising <tengbenkouta@gmail.com>
2025-05-02 15:41:45 +0900

    Initial Commit

---
+++ harrypotterAPIProject/harrypotterAPIProjectApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct harrypotterAPIProjectApp: App {
+    private var router: Router = .launch
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
